e9f044879238be8deaa5285c0793f0cc4c87fc11
KatarinaDojcinovska <katarina.dojcinovska98@gmail.com>
2025-01-10 10:34:06 +0100

    Remove JS hämtat alla element från HTML
    - Alla buttons
    - Input fält och p

---
+++ remove/remove.js
@@ -0,0 +1,6 @@
+const removebutton = document.querySelector("#remove");
+const removeThis = document.querySelector("#removeThis");
+const numbersContainer = document.querySelector(".numbersContainer");
+
+
+const newRandomNumberButton = document.querySelector("#newRandomNumber");
