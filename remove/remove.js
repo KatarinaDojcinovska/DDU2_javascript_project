84e21d0ac4873223bdc654a427fb2d463240eb5e
KatarinaDojcinovska <katarina.dojcinovska98@gmail.com>
2025-01-10 10:39:06 +0100

    Remove addEventListener för newRandomNumberButton
    - När vi klickar på newRandomNumberButton kommer det genereras ett slumpmässigt nummer och skrivas in i numberInput.
    
    -när vi har fått numbret i numberInput kommer en ruta i numbersDiv att bli till färg och markera samma nummer som står i numberInput.

--- remove/remove.js
+++ remove/remove.js
@@ -4,3 +4,19 @@ const numbersContainer = document.querySelector(".numbersContainer");
 
 
 const newRandomNumberButton = document.querySelector("#newRandomNumber");
+newRandomNumberButton.addEventListener("click", function (){
+    const numberInput = document.getElementById("randomNumberSpace");
+    const randomNum = generateRandomNumber(0, 99);
+
+    numberInput.textContent = randomNum;
+    const numbers = document.querySelectorAll(".numberDiv");
+
+    for (const number of numbers) {
+        console.log(number.textContent);
+        if (parseInt(number.textContent) === randomNum){
+            number.classList.add("markTarget");
+        } else {
+            number.classList.remove("markTarget");
+        }
+    }
+});
